7696fd20cb810f14b9362a25f27b12dd2bbd3625
thai-ru <robinthairu34@gmail.com>
2023-12-02 12:23:30 +0300

    dart functions

---
+++ functions.dart
@@ -0,0 +1,9 @@
+void main() {
+  getCurrentDateTime ();
+}
+
+// Simple function to get current date and time.
+void getCurrentDateTime() {
+  var nairobiTime = DateTime.now();
+  print(nairobiTime);
+}
